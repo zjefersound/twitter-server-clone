f987e10ec7566bf8159ffd360bf885392279292b
zjefersound <jeferson.2017317264@gmail.com>
2020-07-20 02:35:05 -0300

    config typeorm criação model e controller user

---
+++ src/app/controllers/UserController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express'
+import { getRepository } from 'typeorm';
+
+import User from '../models/User';
+
+class UserController {
+    async store(request: Request, response: Response){
+        const repository = getRepository(User);
+        const { email, password } = request.body;
+
+        const userExists = await repository.findOne({ where: { email }});
+
+        if (userExists){
+            response.sendStatus(409);
+        }
+
+        const user = repository.create({ email, password });
+        await repository.save(user);
+
+        return response.json(user).sendStatus(201);
+    }
+}
+
+export default new UserController();
