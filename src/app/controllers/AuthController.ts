ba3f76dd006cca0b8394ace68601cf418bdb4142
zjefersound <jeferson.2017317264@gmail.com>
2020-07-21 01:07:57 -0300

    auth controller and auth secret

---
+++ src/app/controllers/AuthController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from 'express'
+import { getRepository } from 'typeorm';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+const { authSecret } = require('../../../.env');
+
+import User from '../models/User';
+
+class AuthController {
+    async authenticate(request: Request, response: Response){
+        const repository = getRepository(User);
+        const { email, password } = request.body;
+
+        const user = await repository.findOne({ where: { email }});
+
+        if (!user){
+            return response.sendStatus(401);
+        }
+        
+        const isValidPassword = await bcrypt.compare(password, user.password);
+        
+        if(!isValidPassword){
+            return response.sendStatus(401);
+        }
+
+        const token = jwt.sign({ id: user.id }, authSecret, { expiresIn: '1d' });
+
+        response.json({
+            user,
+            token
+        });
+    }
+}
+
+export default new AuthController();
